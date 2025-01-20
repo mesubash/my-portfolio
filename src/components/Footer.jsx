273a43291a1164cb84acb7ba72fff03e96e36aec
Subash Singh Dhami <subashdhamee@gmail.com>
2025-01-20 22:51:25 +0545

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// src/components/Footer.jsx
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>©  Subash Singh Dhami - All Rights Reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
