b2d9d2e7dfaa16d85fda4bec9a96a0ddbc6f2125
Subash Singh Dhami <subashdhamee@gmail.com>
2025-01-20 22:49:41 +0545

    About Added

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+// src/components/About.jsx
+import React from "react";
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        I'm a passionate web developer with experience in building modern web
+        applications using React, JavaScript, HTML, and CSS. I love creating
+        beautiful and functional websites.
+      </p>
+      <img src="/assets/profile.jpg" alt="Your Name" />
+    </section>
+  );
+};
+
+export default About;
